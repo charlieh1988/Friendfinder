5a58b9467a7f1d94067f42c75ef9332e7e534c57
Charlie Hilton <charllie.hiltons@gmail.com>
2019-02-24 19:14:59 -0600

    updating files

--- server.js
+++ server.js
@@ -14,9 +14,10 @@ var express = require("express");
 var app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing
+// express.json and express.urlEncoded make it easy for our server to interpret data sent to it.
+// The code below is pretty standard.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -26,15 +27,14 @@ app.use(express.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
-// =============================================================================
+// ==============================================================================
 // LISTENER
 // The below code effectively "starts" our server
-// =============================================================================
+// ==============================================================================
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
- 
